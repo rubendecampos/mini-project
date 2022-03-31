faed91a1f6972924b73d3616d974114b12b781cc
Samuel MICHEL <samuel.michel@idiap.ch>
2022-03-31 10:59:29 +0200

    make analysis more robust

--- analysis.py
+++ analysis.py
@@ -1,4 +1,5 @@
 from sklearn import metrics
+import numpy
 
 
 def analyser(predict,real):
@@ -6,11 +7,23 @@ def analyser(predict,real):
     Perform the absolute error between the prediction of the algorithm
     the real data.
 
-    param train_set: Pandas Dataframe of the train set
-    param test_set: Pandas Dataframe of the test set
+    param train_set: numpy array of the train set
+    param test_set: numpy array Dataframe of the test set
     
     return: a float (the absolute error)
     """ 
+    if(isinstance(predict,'numpy.ndarray')):
+
+        raise TypeError("predict is %s, should be a numpy array"% type(predict))
+
+    if(isinstance(predict,'numpy.ndarray')):
+
+        raise TypeError("real is %s, should be a numpy array"% type(real))
+
+
+    if(predict.shape==real.shape):
+
+        raise IndexError("predict and real should have the same shape and predict have shape %s and real have shape %s"% (predict.shape,real.shape) )
 
     error = metrics.mean_absolute_error(real, predict)
     return error
