2dc81d52117bd625dd69436b6d97e2ef3a6a7485
Samuel MICHEL <samuel.michel@idiap.ch>
2022-04-01 15:33:01 +0200

    settingup setup.py for packaging

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 from setuptools import setup
 
+
 setup(name='saru',
       version='0.1',
       long_description=open("README.rst").read(),
-      url='http://github.com/storborg/funniest',
+      url='https://github.com/rubendecampos/mini-project',
       author='Ruben De CAMPOS and Samuel MICHEL',
       author_email='saru@example.com',
       license='LICENSE.txt',
-      packages=['funniest'],
-      zip_safe=False)
+      packages=find_packages(),
+      package_data={
+        'saru': ['Datasets/*.csv','Datasets/housing.data'],
+      },
+      install_requires=load_requirements('requirements.txt'),
+      entry_points={  
+        'console_scripts': ['saru-run=saru-run:main',],
+        },
+      python_requires=">=3.8, <=3.9",
+      install_requires=["numpy==1.20.3","pandas==1.3.4","scikit-learn==0.24.2"], 
+    )
