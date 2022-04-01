ae0ab3d84b55f47f22baeeec5870b3704386ed5a
Samuel MICHEL <samuel.michel@idiap.ch>
2022-04-01 16:20:16 +0200

    modification of setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
-from setuptools import setup
+import setuptools 
 
 
-setup(name='saru',
+setuptools.setup(name='saru',
       version='0.1',
       long_description=open("README.rst").read(),
       url='https://github.com/rubendecampos/mini-project',
       author='Ruben De CAMPOS and Samuel MICHEL',
       author_email='saru@example.com',
       license='LICENSE.txt',
-      packages=find_packages(),
+      packages=setuptools.find_packages(),
       package_data={
         'saru': ['Datasets/*.csv','Datasets/housing.data'],
       },
-      install_requires=load_requirements('requirements.txt'),
       entry_points={  
-        'console_scripts': ['saru-run=saru-run:main',],
+        'console_scripts': ['saru-run=saru.run:main',],
         },
       python_requires=">=3.8, <=3.9",
       install_requires=["numpy==1.20.3","pandas==1.3.4","scikit-learn==0.24.2"], 
