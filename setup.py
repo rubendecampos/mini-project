e8d78813705d2ae397d6a5bbb0cde997d7a518ad
Samuel MICHEL <samuel.michel@idiap.ch>
2022-04-01 12:44:00 +0200

    adding file for pakaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='saru',
+      version='0.1',
+      long_description=open("README.rst").read(),
+      url='http://github.com/storborg/funniest',
+      author='Ruben De CAMPOS and Samuel MICHEL',
+      author_email='saru@example.com',
+      license='LICENSE.txt',
+      packages=['funniest'],
+      zip_safe=False)
